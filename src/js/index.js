dc7885e782504a0e5d336cf4076474dff4daf2dc
Artis Silins <liltacc7@gmail.com>
2025-02-27 00:01:06 +0200

    pabeigts darbs ar fetch

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,14 @@ function openPopupwindow(){
 function closePopupwindow(){
     popupwindow.classList.remove('open-popupwindow');
 }
+
+document.getElementById('get-fact').addEventListener('click', fetchCatFact);
+
+function fetchCatFact() {
+    fetch('https://catfact.ninja/fact')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('cat-fact').innerText = data.fact;
+        })
+        .catch(error => console.error('Error fetching cat fact:', error));
+    }
